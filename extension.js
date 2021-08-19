e0d94c2a577ea6201fd38fcc3fac659206624335
Pedro Lemos <lemozxpedro@gmail.com>
2021-08-19 18:32:48 -0300

    feat: added a confirmation box before create test

--- extension.js
+++ extension.js
@@ -1,5 +1,6 @@
 const vscode = require('vscode');
 const { createHandler, renameHandler } = require('./src/handlers');
+const { isTestFile } = require('./src/helpers');
 
 function activate(context) {
 	vscode.window.showInformationMessage("Flutter Mimic Active!");
@@ -9,33 +10,42 @@ function activate(context) {
 	watcher.ignoreCreateEvents = false;
 	watcher.ignoreChangeEvents = true;
 	watcher.ignoreDeleteEvents = true;
-	
-	watcher.onDidCreate((e)=>{
-		if(nextActionIsRename){ 
+
+	watcher.onDidCreate((e) => {
+		if (nextActionIsRename) {
 			nextActionIsRename = false;
-		}else{
-			createHandler(e);
-			vscode.window.showInformationMessage("Flutter Mimic: File create!")
+		} else {
+			if (!e.path.endsWith('test.dart')) {
+				vscode.window.showInformationMessage(
+					"Flutter Mimic: You just created a new Dart file. Do you want to create a mirrored test file?",
+					...["Yes", "No"]
+				).then((answer) => {
+					answer === "Yes"
+						? createHandler(e, () => { vscode.window.showInformationMessage("Flutter Mimic: File create!") })
+						: vscode.window.showInformationMessage("Flutter Mimic: Nothing done!")
+
+				})
+			}
 		}
 	})
 
-	vscode.workspace.onDidRenameFiles((e)=>{
+	vscode.workspace.onDidRenameFiles((e) => {
 		nextActionIsRename = true;
-		e.files.forEach((file)=>{
+		e.files.forEach((file) => {
 			renameHandler(file);
 		})
 		vscode.window.showInformationMessage("Adjusment made");
 	})
 
-	vscode.workspace.onDidDeleteFiles((e)=>{
+	vscode.workspace.onDidDeleteFiles((e) => {
 		vscode.window.showInformationMessage("Flutter Mimic: Files arent deleted automatic.");
 	});
 
 	context.subscriptions.push(watcher);
-} 
+}
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
